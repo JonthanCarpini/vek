aaf7f67cbd2c464c23c94fe7aec219a46de30c16
Carpini01 <113296584+Carpini01@users.noreply.github.com>
2026-04-22 10:22:43 -0300

    debug: add logs to server.js startup

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // Servidor customizado Next.js + Socket.io
 // Usa um único processo para servir Next e WebSocket na mesma porta.
+console.log('[Server] Iniciando processo...');
 require('ts-node/register');
 require('tsconfig-paths/register');
+console.log('[Server] ts-node registrado.');
 const { createServer } = require('http');
 const { parse } = require('url');
 const next = require('next');
